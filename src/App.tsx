71bd699e83390ea35d43e74f6b1216b66b44e860
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-11 16:50:43 -0500

    add: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Router>
+    <Routes>
+      <Route path="/" element={<h2>Dashboard</h2>} /> {/* Inicio */}
+      <Route path="/habilidades" element={<h2>Habilidades</h2>} /> {/* Filtrar por habilidades */}
+      <Route path="/apariencia" element={<h2>Apariencia</h2>} /> {/* Filtrar por Apariencia */}
+    </Routes>
+  </Router>
 )
 
 export default App
