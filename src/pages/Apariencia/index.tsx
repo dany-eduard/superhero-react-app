d474a8987434070bd9f9258d7fe4c3d15c441be0
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-11 18:29:28 -0500

    create: Dashboard Apariencia, Habilidades components

---
+++ src/pages/Apariencia/index.tsx
@@ -0,0 +1,16 @@
+const index = () => (
+  <div>
+    <h2>Apariencia</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, eos quidem pariatur accusamus, enim cumque
+      consequatur eveniet minima quod delectus officiis nemo dolores nulla rerum, unde non. Repudiandae aliquam
+      perferendis eius provident aperiam obcaecati ipsum temporibus maxime distinctio. Libero, quas aperiam! Distinctio,
+      ad quam. Fugiat sequi reiciendis cum excepturi cupiditate! Facilis perferendis laborum vero suscipit fugiat qui,
+      sint mollitia. Necessitatibus quo ipsum atque corporis quia architecto iste magni alias voluptas consequuntur id
+      quas impedit nesciunt veniam amet explicabo quisquam, excepturi tenetur quis ratione reiciendis sint veritatis!
+      Repellendus provident, quas, id ut nesciunt dolores quidem, itaque explicabo dicta quos ratione nemo!
+    </p>
+  </div>
+)
+
+export default index
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+const index = () => (
+  <div>
+    <h1>Dashboard</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptatibus eos facilis repudiandae iusto enim ipsa
+      distinctio! Impedit officiis reiciendis earum natus mollitia, rem eligendi exercitationem neque. Ipsa culpa
+      quaerat praesentium soluta fugit labore hic voluptatibus quo a, similique, ratione eligendi, ducimus qui.
+      Reprehenderit deleniti neque distinctio. Nisi excepturi laboriosam commodi necessitatibus numquam magni libero
+      esse, nam dignissimos, fugit, voluptate incidunt optio saepe enim eveniet corporis inventore praesentium nihil
+      quas consequatur maiores eaque qui! Cum at maiores deleniti laborum dolorum dicta, fugiat vel eveniet quisquam
+      dolores voluptatum molestiae, voluptas nihil quaerat exercitationem quia voluptatem nam hic doloremque ipsam
+      obcaecati. Ratione.
+    </p>
+  </div>
+)
+
+export default index
