aeccdf4e5b47f15efaa639944dddc11f9b34c51c
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-11 18:29:46 -0500

    add: base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import Layout from 'components/Layout'
+import Dashboard from 'pages/Dashboard'
+import Habilidades from 'pages/Habilidades'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => (
   <Router>
-    <Routes>
-      <Route path="/" element={<h2>Dashboard</h2>} /> {/* Inicio */}
-      <Route path="/habilidades" element={<h2>Habilidades</h2>} /> {/* Filtrar por habilidades */}
-      <Route path="/apariencia" element={<h2>Apariencia</h2>} /> {/* Filtrar por Apariencia */}
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Dashboard />} /> {/* Inicio */}
+        <Route path="/habilidades" element={<Habilidades />} /> {/* Filtrar por habilidades */}
+        <Route path="/apariencia" element={<h2>Apariencia</h2>} /> {/* Filtrar por Apariencia */}
+      </Routes>
+    </Layout>
   </Router>
 )
 
