529e170c77ce29646aae58d66f9f3a79dbe1ce90
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-11-15 00:17:16 -0500

    create: constants to select skills

---
+++ src/constants/constantsHero.ts
@@ -0,0 +1,37 @@
+export const SelectPowerstatsOptions = [
+  {
+    label: 'Inteligencia',
+    value: 'intelligence',
+  },
+  {
+    label: 'Fuerza',
+    value: 'strength',
+  },
+  {
+    label: 'Velocidad',
+    value: 'speed',
+  },
+  {
+    label: 'Durabilidad',
+    value: 'durability',
+  },
+  {
+    label: 'Poder',
+    value: 'power',
+  },
+  {
+    label: 'Combate',
+    value: 'combat',
+  },
+]
+
+export const SortOptions = [
+  {
+    label: 'Ascendente',
+    value: 'asc',
+  },
+  {
+    label: 'Descendente',
+    value: 'desc',
+  },
+]
